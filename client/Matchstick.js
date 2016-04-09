a4c066b386d039a37f7e4ca1978ad83c15511770
Mike Schaeffer <michael.schaeffer@lab49.com>
2016-04-09 16:54:33 -0400

    Add support for toggling sticks.

--- client/Matchstick.js
+++ client/Matchstick.js
@@ -51,14 +51,33 @@ export default class Matchstick extends Component {
         this.state = { board };
     }
 
+    getStickClickHandler(spos, side) {
+        return () => {
+            console.error("click", spos, side);
+            
+            let board = this.state.board;
+
+            let oldStick = getMatchStick(board, spos, side);
+
+            this.setState({ board: setMatchStick(board, spos, side, !oldStick)});
+        };
+    }
+    
     makeHRow(board, y, side) {
         let row = [];
 
         let sx = board.get('sx');
         
         for(let x = 0; x < sx; x++) {
+            let spos = { x, y };
+            
             row.push(<td key={x + "-l"}/>);
-            row.push(<td key={x + "-m"} className={classNames('h-matchstick', {'placed': getMatchStick(board, { x, y }, side)})}/>);
+            
+            row.push(<td key={x + "-m"}
+                     onClick={this.getStickClickHandler(spos, side)}
+                     className={classNames('h-matchstick', {
+                         'placed': getMatchStick(board, spos, side)
+                     })}/>);
         }
         
         row.push(<td key={sx + "-r"}/>);
@@ -72,11 +91,22 @@ export default class Matchstick extends Component {
         let sx = board.get('sx');
         
         for(let x = 0; x < sx; x++) {
-            row.push(<td key={x + "-l"} className={classNames('v-matchstick', {'placed': getMatchStick(board, { x, y }, 'left')})}/>);                
+            let spos = { x, y };
+            
+            row.push(<td key={x + "-l"}
+                     onClick={this.getStickClickHandler(spos, 'left')}
+                     className={classNames('v-matchstick', {
+                         'placed': getMatchStick(board, spos, 'left')
+                     })}/>);
+            
             row.push(<td key={x + "-m"}/>);
         }
 
-        row.push(<td key={sx + "-r"} className={classNames('v-matchstick', {'placed': getMatchStick(board, { x : sx, y }, 'left')})}/>);                
+        row.push(<td key={sx + "-r"}
+                 onClick={this.getStickClickHandler({ x : sx, y}, 'left')}
+                 className={classNames('v-matchstick', {
+                     'placed': getMatchStick(board, { x : sx, y }, 'left')
+                 })}/>);
         
         return <tr key={y + "-v"} className="vrow">{row}</tr>;
     }
