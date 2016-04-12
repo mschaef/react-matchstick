f9dd8c3cdb66c63a1cd4ff12a2bf96a9025ac3c9
Mike Schaeffer <michael.schaeffer@lab49.com>
2016-04-12 15:01:21 -0400

    Add a more difficult test case to the solver.

--- client/MatchstickGame.js
+++ client/MatchstickGame.js
@@ -36,6 +36,7 @@ export default class MatchstickGame extends Component {
     getDefaultBoard() {
         let board = createBoard(4, 4);
 
+        /*
         board = setMatchStick(board, 0, 1, SIDE_LEFT, true);
         board = setMatchStick(board, 1, 1, SIDE_LEFT, true);        
         board = setMatchStick(board, 0, 1, SIDE_TOP , true);
@@ -46,6 +47,28 @@ export default class MatchstickGame extends Component {
         board = setMatchStick(board, 1, 0, SIDE_TOP , true);
         board = setMatchStick(board, 2, 0, SIDE_TOP , true);
         board = setMatchStick(board, 2, 1, SIDE_TOP , true);        
+         */
+
+        board = setMatchStick(board, 0, 0, SIDE_LEFT, true);
+        board = setMatchStick(board, 0, 1, SIDE_LEFT, true);
+        board = setMatchStick(board, 0, 2, SIDE_LEFT, true);
+
+        board = setMatchStick(board, 0, 3, SIDE_TOP, true);
+        board = setMatchStick(board, 1, 3, SIDE_TOP, true);
+        board = setMatchStick(board, 2, 3, SIDE_TOP, true);
+
+        board = setMatchStick(board, 1, 0, SIDE_LEFT, true);
+        board = setMatchStick(board, 1, 1, SIDE_LEFT, true);
+
+        board = setMatchStick(board, 1, 2, SIDE_TOP, true);
+        board = setMatchStick(board, 2, 1, SIDE_LEFT, true);
+        
+        board = setMatchStick(board, 3, 0, SIDE_LEFT, true);
+        board = setMatchStick(board, 3, 1, SIDE_LEFT, true);
+        board = setMatchStick(board, 3, 2, SIDE_LEFT, true);
+
+        board = setMatchStick(board, 1, 0, SIDE_TOP, true);
+        board = setMatchStick(board, 2, 0, SIDE_TOP, true);        
         
         return board;
     }
